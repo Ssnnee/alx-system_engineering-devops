83e19ce3787657852ca4c9a23ee916a75ed5726f
Ssnnee <nandisamuelsne@gmail.com>
2023-10-03 18:19:33 -0700

    The or symbol

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# This script implement a regular expression that match School
+
+
+if ARGV.length != 1
+  exit 1
+end
+
+input = ARGV[0]
+
+regex = /htn|hbtn/
+matches = input.scan(regex)
+
+
+if matches.empty?
+  puts ""
+else
+  matches.each do |match|
+    puts "#{match}"
+  end
+end
