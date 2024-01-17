9c8b2206dd3d237e26e1a838b2f0f52154734345
Ssnnee <nandisamuelsne@gmail.com>
2024-01-17 10:33:50 +0100

    fix:

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,9 +7,8 @@ import requests
 def number_of_subscribers(subreddit):
     """This function returns the number of subscribers of a given subreddit """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
 
-    res = requests.get(url, headers=headers, allow_redirects=False)
+    res = requests.get(url, allow_redirects=False)
 
     if res.status_code != 200:
         return 0
