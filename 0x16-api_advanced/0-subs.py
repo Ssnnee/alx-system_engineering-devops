6235e25520ef93fba36283439e70e1c990d5489c
Ssnnee <nandisamuelsne@gmail.com>
2024-01-17 10:39:54 +0100

    fix: trying to make it return the good value

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,22 @@ import requests
 def number_of_subscribers(subreddit):
     """This function returns the number of subscribers of a given subreddit """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
 
-    res = requests.get(url, allow_redirects=False)
+    res = requests.get(url, headers=headers, allow_redirects=False)
 
     if res.status_code != 200:
         return 0
 
-    nbr_subs = json.loads(res.text).get("data").get("subscribers")
-    return nbr_subs
+    data = json.loads(res.text)
+
+    if data is None:
+        return 0
+
+    if "data" not in data:
+        return 0
+
+    if "subscribers" not in data.get("data"):
+        return 0
+
+    return data.get("data").get("subscribers")
