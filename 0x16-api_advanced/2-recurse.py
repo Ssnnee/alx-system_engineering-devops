49a484f8f3731613f000a595144f360996d3a4d7
Ssnnee <nandisamuelsne@gmail.com>
2024-01-17 10:09:07 +0100

    feat: return a list containing the titles using recursive function

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module contains a recursive function that queries the Reddit API """
+import requests
+import json
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """This function returns a list containing the titles of all hot articles
+    for a given subreddit.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    params = {'limit': 100}
+
+    if after:
+        params['after'] = after
+
+    res = requests.get(url, params=params, allow_redirects=False,)
+
+    if res.status_code != 200:
+        return None
+
+    data = json.loads(res.text)
+
+    for i in range(len(data['data']['children'])):
+        hot_list.append(data['data']['children'][i]['data']['title'])
+    if 'after' in data['data']:
+        recurse(subreddit, hot_list, data['data']['after'])
+    return hot_list
